df822519a0d0747adb342a4347a5339aedd6c60f
Chantal <esweinchantal7@gmail.com>
2025-02-22 20:54:24 +0100

    fehler beim löschen von würfeln

--- wuerfel.js
+++ wuerfel.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+	let progress_index = 0;
+
 	// add and remove buttons click event
 	document.querySelectorAll('#dices button').forEach(button => {
 		button.addEventListener('click', e => {
@@ -11,12 +13,48 @@ window.onload = function () {
 
 	// Auf den Würfeln-Button klicken
 	document.querySelector('#roll button').addEventListener('click', e => {
-		// Bei jedem Würfel eine Zahl würfeln
+		let diceArea = document.querySelector('#dice-area'), // dice-area (Html-Element) holen
+			dices = diceArea.querySelectorAll('.area-dice'), // alle würfel unter dice-area holen
+			progress = document.querySelector('#progress'), // progress (Html-Element) holen
+			result = 0;
 
-		// Die würfel im Verlauf speichern
+		let element = document.createElement('div'); // div-Element erstellen
+		element.classList.add('progress-line'); // div-Element Klasse hinzufügen (progress-line)
+		element.setAttribute('data-index', progress_index.toString()); // div-Element ein Attribut geben und in einen Sting umwandeln
+		progress_index++; // index um 1 erhöhen
 
-		// Ergebnis über dem Würfeln-Button anzeigen
+		dices.forEach(dice => { // alle Würfel unter dice-area durchgehen
+			// random Zahl wurde geholt (min = 1, max = Typ des Würfels (wird in eine Ganzzahl umgewandelt))
+			let randomNumber = getRandomNumber(1, parseInt(dice.getAttribute('data-type')));
+			result += randomNumber;
+
+			// Attribut wird hinzugefügt namens data-rolled
+			// und mit dem Wert random Zahl welche in ein String umgewandelt wird
+			dice.setAttribute('data-rolled', randomNumber.toString());
+
+			// Würfel-Html-Element wird kopiert und in das div-Element als Kind am Ende eingefügt
+			element.appendChild(dice.cloneNode(true));
+		})
+
+		progress.setAttribute('data-result', result);
+		// Element-Html-Element mit samt den würfeln wir in das Progress-Fenster eingefügt
+		progress.appendChild(element);
+
+		// Ausgabe zwischen Dice-Area und Würfel-Button
+		let result_div = document.querySelector('#result');
+		result_div.innerHTML = result.toString();
 	})
+}
+
+/**
+ * Eine random Number mit min und max holen
+ *
+ * @param min
+ * @param max
+ * @returns {*}
+ */
+function getRandomNumber(min, max) {
+	return Math.random() * (max - min) + min;
 }
 
 /**
@@ -30,6 +68,7 @@ function removeOrAddDice (dice, type) {
 	let dice_area = document.querySelector('#dice-area');
 
 	if (type === "remove") {
+
 		let elem = dice_area.querySelector(".dice-area[data-type='" + dice + "']");
 
 		if (elem) {
@@ -54,7 +93,7 @@ function removeOrAddDice (dice, type) {
  */
 function createHtmlDice (type) {
 	let div = document.createElement('div');
-	div.classList.add('dice-area');
+	div.classList.add('area-dice');
 	div.setAttribute('data-type', type);
 
 	return div;
